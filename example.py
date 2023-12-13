b0a16a5dc67ab2ad6ea423e122934dc55237b612
Gnomeslayer <Declan.otten@live.com.au>
2023-12-14 00:21:57 +1100

    Update example.py

--- example.py
+++ example.py
@@ -2,38 +2,24 @@ from battlemetrics import Battlemetrics
 import json
 import asyncio
 
-with open("config.json", "r") as f:
-    config = json.load(f)
-
-battlemetrics_token = config['tokens']['battlemetrics_token']
-
 example_bmid = 579979635
 example_steam_id = 76561198151275725
 example_organization_id = 25433
 example_banlist_id = "abc"
 example_server_id = 1234
 
-bmapi = Battlemetrics(battlemetrics_token)
+bmapi = Battlemetrics("Your battlemetrics token here.")
 
-#example = asyncio.run(api.organization_friend_list(organization_id=example_organization_id))
-
-#example = asyncio.run(api.ban_list_search(banlist=example_banlist_id))
-
-#example = asyncio.run(api.server_info(server_id=example_server_id))
-
-example = asyncio.run(bmapi.player.player_info(example_bmid))
-
-#example = asyncio.run(api.player_list(filter_online=True))
-
-#example = asyncio.run(api.leaderboard_info(server_id=example_server_id))
-
-#example = asyncio.run(api.player_match_identifiers(identifier=example_steam_id, type="steamID"))
-
-#example = asyncio.run(api.player_quick_match(identifier=example_steam_id, type="steamID"))
-
-#example = asyncio.run(api.server_list(server_type=['community'], rcon=False))
-
-#example = asyncio.run(api.server_list(server_type=['modded', 'official'], rcon=False))
+example = asyncio.run(bmapi.organization.info(organization_id=example_organization_id))
+example = asyncio.run(bmapi.ban_list.search(banlist=example_banlist_id))
+example = asyncio.run(bmapi.server.info(server_id=example_server_id))
+example = asyncio.run(bmapi.player.search(example_bmid))
+example = asyncio.run(bmapi.player.search(filter_online=True))
+example = asyncio.run(bmapi.server.leaderboard_info(server_id=example_server_id))
+example = asyncio.run(bmapi.player.match_identifiers(identifier=example_steam_id, type="steamID"))
+example = asyncio.run(bmapi.player.quick_match(identifier=example_steam_id, type="steamID"))
+example = asyncio.run(bmapi.server.search(server_type=['community'], rcon=False))
+example = asyncio.run(bmapi.server.search(server_type=['modded', 'official'], rcon=False))
 
 with open('example.json', 'w') as f:
     f.write(json.dumps(example, indent=4))
