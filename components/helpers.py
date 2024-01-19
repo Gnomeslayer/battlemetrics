5de43a48fa50d791384121ae5dfa475025676384
Declan <declan.otten@live.com.au>
2024-01-19 20:31:19 +1100

    Updated request function

--- components/helpers.py
+++ components/helpers.py
@@ -70,12 +70,13 @@ class Helpers:
         else:
             return None
 
-    async def _make_request(self, method: str, url: str, data: dict = None) -> dict:
+    async def _make_request(self, method: str, url: str, params: dict = None, json:dict= None) -> dict:
         """Queries the API and spits out the response.
         Args:
             method (str): One of: GET, POST, PATCH, DELETE
             url (str): The endpoint/url you wish to query.
-            data (dict, optional): Any params or json data you wish to send to enhance your experience?. Defaults to None.
+            params (dict, optional): Any params you wish to send to enhance your experience?. Defaults to None.
+            json (dict, optional): json data you wish to send to enhance your experience?. Defaults to None.
         Raises:
             Exception: Doom and gloom.
         Returns:
@@ -83,41 +84,36 @@ class Helpers:
         """
 
         async with aiohttp.ClientSession(headers=self.headers) as session:
-            response = None
-            response_content = None
-            response_status = None
-            content_type = None
-            if method in ["POST", "PATCH"]:
-                async with session.request(method=method, url=url, json=data) as r:
+            async with session.request(method=method, url=url, json=json, params=params) as r:
+                if r:
                     response_content = await r.content.read()
                     response_status = r.status
                     content_type = r.headers.get('content-type', '')
-            else:
-                async with session.request(method=method, url=url, params=data) as r:
-                    response_content = await r.content.read()
-                    response_status = r.status
-                    content_type = r.headers.get('content-type', '')
+                else:
+                    print("Some error message here?")
 
-            if response_status == '429':
-                print(
-                    "You're being rate limited by the API. Please wait a minute before trying again.")
-                return
-
-            if 'json' in content_type:
-                try:
-                    response = await response_content.json()
-                except:
-                    response = self.exception_handler(response_content)
-            elif 'octet-stream' in content_type:
-                response = await self._parse_octet_stream(await r.content.read())
-            elif "text/html" in content_type:
-                response = await r.text()
-                response = response.replace("'", "").replace("b", "")
-            else:
-                raise Exception(
-                    f"Unsupported content type: {content_type}")
+        if response_status == '429':
+            print("You're being rate limited by the API. Please wait a minute before trying again.")
+            return
+        if 'json' in content_type:
+            #Try and convert the response to something we can handle.
+            try:
+                response = await response_content.json()
+            except:
+                #Attempt to fix any errors in the json response.
+                response = await self.exception_handler(response_content)
+        #This is dedicated if the user is attempting to download a banlist. Currently only allows RUST banlists.
+        elif 'octet-stream' in content_type:
+            response = await self._parse_octet_stream(await response.content.read())
+        #Sometimes the API returns HTML responses. Lets handle those.
+        elif "text/html" in content_type:
+            response = await response.text()
+            response = response.replace("'", "").replace("b", "")
+        else:
+            raise Exception(f"Unsupported content type: {content_type}")
         return response
 
+    #This function attempts to find and fix any errors in the JSON response.
     async def exception_handler(self, response_content):
         json_string = response_content.decode('utf-8')
         json_dict = None
